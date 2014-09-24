bf3b6d9276741c129821b1fa876972d381001a9b
Chenjian0930 <chenjian@lianchuang.com>
2014-09-24 17:44:38 +0800

    test func

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var button:UIButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        button.frame = CGRectMake(100, 100, 100, 100)
+        button.backgroundColor = UIColor.redColor()
+        button.addTarget(self, action: "test:", forControlEvents: UIControlEvents.TouchUpInside)
+        button.setTitle("touch", forState: UIControlState.Normal)
+        self.view.addSubview(button)
+        
+        
+        var iamge:UIImage = UIImage(named: "")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +31,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func test(sender:AnyObject) ->Void{
+        println("test")
+        let timer:NSTimer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "", userInfo: nil, repeats: true)
+    }
+    
+    func timeTest()->Void{
+        
+    }
+   
 }
 
