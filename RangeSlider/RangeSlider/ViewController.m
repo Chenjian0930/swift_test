ba88f678d091ff7edac8a8208c89f8f81d701d66
Chenjian0930 <chenjian@lianchuang.com>
2014-10-15 10:50:03 +0800

    。。。

---
+++ RangeSlider/RangeSlider/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  RangeSlider
+//
+//  Created by chen on 14-10-13.
+//  Copyright (c) 2014年 chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *testView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
