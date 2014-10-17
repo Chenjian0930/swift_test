65fef1b65aa57e9e77270599cea5c9762da2fffb
Chenjian0930 <chenjian@lianchuang.com>
2014-10-17 16:18:40 +0800

    test

---
+++ Test/Test/MyPlayground.playground/section-1.swift
@@ -0,0 +1,21 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var a:Int = 2
+println(-a)
+
+
+let b :Float = 3.1
+let c :Int = 4
+let sum = b + Float(c)
+
+
+var student1 = ("1001","张三",20,90)
+println("学生\(student1.1)")
+
+var student2 = (id:"1002",name:"李四")
+println("学生\(student2.name)")
