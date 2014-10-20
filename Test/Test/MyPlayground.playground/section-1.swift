e0148da3c717f54c741056eadf65cd95345a83ac
Chenjian0930 <chenjian@lianchuang.com>
2014-10-20 15:14:09 +0800

    test swift

--- Test/Test/MyPlayground.playground/section-1.swift
+++ Test/Test/MyPlayground.playground/section-1.swift
@@ -2,6 +2,8 @@
 
 import UIKit
 
+
+
 var str = "Hello, playground"
 
 
@@ -19,3 +21,44 @@ println("学生\(student1.1)")
 
 var student2 = (id:"1002",name:"李四")
 println("学生\(student2.name)")
+
+
+var i = 1
+while (i*i < 100000){
+    i++
+}
+
+
+
+func calculate (fuhao:String) ->(Int,Int)->Int{
+    func jiahao(a:Int,b:Int)->Int{
+        return a+b
+    }
+    func jianhao(a:Int,b:Int)->Int{
+        return a-b
+    }
+    var result:(Int,Int)->Int
+    if(fuhao == "+"){
+        result =  jiahao
+        return result
+    }
+    else if(fuhao == "-"){
+        result = jianhao
+        return result
+    }
+    
+    switch (fuhao){
+    case "+":
+        result =  jiahao
+    case "-":
+        result = jianhao
+    default:
+        result = jianhao
+        
+    }
+    return result
+}
+
+
+var fun1:(Int,Int)->Int = calculate("+")
+fun1(1,2)
